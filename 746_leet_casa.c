84d8d377d879e0cb3e07cdbf6b598f1604fbe006
SandJunior <114614503+SandJunior@users.noreply.github.com>
2023-07-02 23:56:49 -0300

    Update 746_leet_casa.c

--- 746_leet_casa.c
+++ 746_leet_casa.c
@@ -1,3 +1,5 @@
+//não consegui descobrir o porquê de constar como wrong answer, não sei onde tá o problema :(
+
 int minCost(int x, int y){
     if (x > y){
         return y;
@@ -10,7 +12,7 @@ int minCostClimbingStairs(int *cost, int costSize)
     int *costTwo = (int *)malloc(costSize * sizeof(int));
     for (int i = costSize - 3; i >= 0; i--)
     {
-        costTwo[i] = cost[i] + minCost(cost[i + 1], cost[i + 2]);
+        costTwo[i] = cost[i] + minCost(costTwo[i + 1], costTwo[i + 2]);
     }
     return minCost(costTwo[0], costTwo[1]);
 }
